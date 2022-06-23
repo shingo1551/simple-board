3bdcdda4b26c54337632848286d3da016c136772
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-06-23 12:32:50 +0900

    connectedCallback

--- stencil-board/src/components/app-home/app-home.tsx
+++ stencil-board/src/components/app-home/app-home.tsx
@@ -6,6 +6,11 @@ import { Component, h, Host } from '@stencil/core';
   shadow: true,
 })
 export class AppHome {
+  connectedCallback() {
+    document.title = 'Simple Board | Home';
+    const description = document.querySelector('meta[name="Description"]') as HTMLMetaElement;
+    description.content = 'DESCRIPTION';
+  }
 
   render() {
     return (
