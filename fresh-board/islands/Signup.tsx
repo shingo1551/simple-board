cd46eced281db6e7256f1271e25b2d60b7002f6f
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-09-07 22:51:46 +0900

    signup

--- fresh-board/islands/Signup.tsx
+++ fresh-board/islands/Signup.tsx
@@ -1,44 +1,67 @@
 /** @jsx h */
 import { ComponentChildren, h } from "preact";
-import { useRef, useState } from 'preact/hooks';
+import { useRef } from 'preact/hooks';
+import { signal } from "@preact/signals";
+
+import { fetchCors } from '../shared/fetch.ts';
+
+const err = signal({
+  error: null,
+  email: false,
+  pass1: false,
+  pass2: false,
+  disabled: true
+});
 
 export default function Signup() {
-  const email = useRef(null);
-  const passwd1 = useRef(null);
-  const passwd2 = useRef(null);
-
-  const [error, setError] = useState(null);
-  const [errEmail, setErrEmail] = useState(false);
-  const [errPass1, setErrPasswd1] = useState(false);
-  const [errPass2, setErrPasswd2] = useState(false);
-  const [disabled, setDisabled] = useState(false);
+  const email = useRef<HTMLInputElement>(null);
+  const passwd1 = useRef<HTMLInputElement>(null);
+  const passwd2 = useRef<HTMLInputElement>(null);
 
   const onEmail = () => {
-    setErrEmail(!email);
-    buttonDisabled();
+    err.value.email = !email;
+    onPasswd1();
   };
 
   const onPasswd1 = () => {
-    setErrPasswd1(!passwd1);
+    err.value.pass1 = !passwd1.current?.value;
     onPasswd2();
   };
 
   const onPasswd2 = () => {
-    setErrPasswd2(passwd1 !== passwd2);
+    err.value.pass2 = passwd1.current?.value !== passwd2.current?.value;
     buttonDisabled();
   };
 
-  const onSignUp = () => { };
-
   const buttonDisabled = () => {
-    setDisabled(errEmail || errPass1 || errPass2 || !passwd1);
+    const value = err.value;
+    err.value = {
+      ...value,
+      error: null,
+      disabled: value.email || value.pass1 || value.pass2 || !email.current?.value
+    }
   };
+
+  const onSignUp = async (evt: Event) => {
+    evt.preventDefault();
+
+    const body = {
+      email: email.current?.value,
+      passwd: passwd1.current?.value
+    }
+    try {
+      await fetchCors('sign-up', 'post', body);
+      location.href = '/signin';
+    } catch (e) {
+      err.value = { ...err.value, error: e };
+    }
+  }
 
   return (
     <form class="signup">
       <h1>Sign up</h1>
       <div>
-        <Error>{error}</Error>
+        <Error>{err.value.error}</Error>
         <input ref={email} onInput={onEmail} autocomplete="username" placeholder="jane@example.com" />
         {/* <span class='error'>This field is required.</span> */}
         <input ref={passwd1} onInput={onPasswd1} type="password" autocomplete="new-password" placeholder="password" />
@@ -47,7 +70,7 @@ export default function Signup() {
         {/* <span class='error'>This field is required.</span> */}
       </div>
       <hr />
-      <button onClick={onSignUp} disabled={disabled}>Sign up</button>
+      <button onClick={onSignUp} disabled={err.value.disabled}>Sign up</button>
     </form>
   );
 }
