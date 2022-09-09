4d0c3ba87103f21a8875ad00eb7b897a1b98b383
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-09-09 20:49:40 +0900

    HTMLAttributes

--- fresh-board/components/Button.tsx
+++ fresh-board/components/Button.tsx
@@ -1,6 +1,7 @@
+import { JSX } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
-export function Button(props: h.JSX.HTMLAttributes<HTMLButtonElement>) {
+export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
