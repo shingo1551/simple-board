39ce1ee7bb5245840acbfa2c5fa76f5e1163cdd1
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-09-19 01:13:45 +0900

    .env.ts

--- fresh-board/shared/postgres.ts
+++ fresh-board/shared/postgres.ts
@@ -1,20 +1,5 @@
 import { Client } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
-
-const config = {
-  database: "postgres",
-  hostname: "db.uffutgvhycnzromcrulu.supabase.co",
-  password: "Hodogaya502",
-  port: 5432,
-  user: "postgres",
-  tls: {
-    caCertificates: [
-      await Deno.readTextFile(
-        new URL("../prod-ca-2021.crt", import.meta.url),
-      ),
-    ],
-    enabled: false,
-  },
-}
+import { config } from "../shared/.env.ts"
 
 export const client = new Client(config);
 await client.connect();
